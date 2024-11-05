fd0c62ed94d787e62733f44de6efd35ae71b961c
Konstantinos Gerogiannis <kostger2@gmail.com>
2024-11-05 15:57:27 +0100

    Project setup

---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <section className="w-full nav-height bg-black relative">
+      <div className="h-5/6 w-full flex justify-center flex-col ">
+        <p className="hero-title">iPhone 16 Pro</p>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
