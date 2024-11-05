e3cfff991ee9db913a971d3e9c0aed1c4f73984e
Konstantinos Gerogiannis <kostger2@gmail.com>
2024-11-05 16:20:01 +0100

    Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,64 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { heroVideo, smallHeroVideo } from "../utils";
 function Hero() {
+  const [videoSrc, setVideoSrc] = useState(
+    window.innerWidth > 768 ? heroVideo : smallHeroVideo
+  );
+  const handleVideroSrcSet = () => {
+    if (window.innerWidth < 768) {
+      setVideoSrc(smallHeroVideo);
+    } else {
+      setVideoSrc(heroVideo);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener("resize", handleVideroSrcSet);
+    return () => {
+      window.removeEventListener("resize", handleVideroSrcSet);
+    };
+  }, []);
+  useGSAP(() => {
+    gsap.to("#hero", {
+      duration: 1,
+      opacity: 1,
+      y: -20,
+      ease: "power4.out",
+      delay: 1.5,
+    });
+    gsap.to("#cta", {
+      opacity: 1,
+      y: -50,
+      delay: 2,
+    });
+  }, []);
   return (
     <section className="w-full nav-height bg-black relative">
-      <div className="h-5/6 w-full flex justify-center flex-col ">
-        <p className="hero-title">iPhone 16 Pro</p>
+      <div className="h-5/6 w-full flex-center flex-col ">
+        <p id="hero" className="hero-title">
+          iPhone 16 Pro
+        </p>
+        <div className="md:w-10/12 w-9/12">
+          <video
+            className="pointer-events-none"
+            autoPlay
+            muted
+            playsInline={true}
+            key={videoSrc}
+          >
+            <source src={videoSrc} type="video/mp4" />
+          </video>
+        </div>
+      </div>
+      <div
+        id="cta"
+        className="flex flex-col items-center opacity-0 translate-y-20"
+      >
+        <a href="#highlights" className="btn">
+          Buy
+        </a>
+        <p className="font-normal text-xl">From $199/month or $999</p>
       </div>
     </section>
   );
